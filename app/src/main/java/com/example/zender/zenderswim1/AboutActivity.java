1a7274b7ea8e9391e834892fed58930cc5fe9028
pzender <ememek96@gmail.com>
2017-04-04 21:11:24 +0200

    Fixed About Title

--- app/src/main/java/com/example/zender/zenderswim1/AboutActivity.java
+++ app/src/main/java/com/example/zender/zenderswim1/AboutActivity.java
@@ -12,6 +12,7 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_about);
         Toolbar toolbar = (Toolbar) findViewById(R.id.tb_more);
         setSupportActionBar(toolbar);
